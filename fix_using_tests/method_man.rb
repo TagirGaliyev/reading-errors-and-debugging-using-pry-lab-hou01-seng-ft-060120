81ceee7c9a98e7ce56c96065eaa1c542179194a7
Tagir Galiyev <w.tagir.galiyev@gmail.com>
2020-05-13 18:13:20 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,8 @@
 # don't forget to add: require 'pry'
-require "pry"
+
 def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
 end
-binding.pry
 
 def play_game(player2, player1)
   "#{player2} is better than #{player1}"
