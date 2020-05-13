29e9d99a9e1afc153d1448acc189107b10792a2c
Tagir Galiyev <w.tagir.galiyev@gmail.com>
2020-05-13 18:32:58 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,7 +2,9 @@
 require "pry"
 def snake_it_up(string)
   if string[0] == "s"
-    10.times do "s" + string end
+    10.times do
+      string = "s" + string
+    end
     string
   else
     string
